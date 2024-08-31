7e4907c1c6a1f69ddcbbfebcf51949d53ae77cc5
sanika-bhor <bhorsanika0239@gmail.com>
2024-08-31 11:33:27 +0530

    oop class, static, object

---
+++ First-App/src/Entity/Services/OOPConcept.js
@@ -0,0 +1,26 @@
+class OOPConcept {
+  static Addition(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+    return this.num1 + this.num2;
+  }
+
+  static Subtraction(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+    return this.num1 - this.num2;
+  }
+
+  static Multiplication(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+    return this.num1 * this.num2;
+  }
+
+  static Division(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+    return this.num1 / this.num2;
+  }
+}
+export default OOPConcept;
--- First-App/src/Main/Components/home.js
+++ First-App/src/Main/Components/home.js
@@ -1,5 +1,19 @@
-
+import OOPConcept from "../../Entity/Services/OOPConcept";
 function Home() {
+
+  const performOpertaion =()=>
+  {
+    let Additon=OOPConcept.Addition(10,20);
+    let Subtraction = OOPConcept.Subtraction(30,20);
+    let Multiplication = OOPConcept.Multiplication(5, 2);
+    let Division = OOPConcept.Division(10, 2);
+
+
+    console.log("Addition: "+Additon);
+    console.log("Subtraction: "+Subtraction);
+    console.log("multiplication: "+Multiplication);
+    console.log("division: "+Division);
+  }
   return (
     <div className="Home">
      
@@ -13,6 +27,8 @@ function Home() {
               <p>💰Net Banking Solution💰</p>
             </i>
           </b>
+
+          <button onClick={performOpertaion}>Invoke OOP concept</button>
         </center>
       </div>
     </div>
