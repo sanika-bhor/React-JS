c14fd8826122352906803c1e902e4f65cef70479
sanika-bhor <bhorsanika0239@gmail.com>
2024-08-31 11:59:55 +0530

    circle is a shape( is a relation ship)

--- First-App/src/Entity/Services/Drawing/circle.js
+++ First-App/src/Entity/Services/Drawing/circle.js
@@ -1,1 +1,16 @@
 
+import Shape from "./shape";
+class Circle extends Shape {
+  constructor(p1, radius, color, thickness) {
+    super(color, thickness);
+    this.center = p1;
+    this.radius = radius;
+  }
+
+  draw() {
+    super.draw();
+    console.log("center point: " + this.center);
+    console.log("radius: " + this.radius);
+  }
+}
+export default Circle;
