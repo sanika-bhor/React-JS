fbf0ae4b52853e276294200e64b1c35ca47a5eb1
sanika-bhor <bhorsanika0239@gmail.com>
2024-08-31 11:58:39 +0530

    line is a shape( is a relation ship)

--- First-App/src/Entity/Services/Drawing/line.js
+++ First-App/src/Entity/Services/Drawing/line.js
@@ -1,1 +1,15 @@
+import Shape from "./shape";
+class Line extends Shape{
+  constructor(p1,p2,color, thickness) {
+    super(color,thickness);
+    this.startPoint = p1;
+    this.endPoint = p2;
+  }
 
+  draw() {
+    super.draw();
+    console.log("Start point: " + this.startPoint);
+    console.log("End Point: " + this.endPoint);
+  }
+}
+export default Line;
