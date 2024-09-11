168485fd07461a2cecfa8dcc5a9b37a9f828957d
sanika-bhor <bhorsanika0239@gmail.com>
2024-09-11 23:02:49 +0530

    get flowers data from json object

--- props_state/src/App.js
+++ props_state/src/App.js
@@ -5,6 +5,8 @@ function App() {
 
   var product=[
     {productName:"lotus", discription:"beautiful flower",price:50, stock:89 },
+    {productName:"rose", discription:"red flower",price:25, stock:130 },
+    {productName:"sunflower", discription:"yellow flower",price:10, stock:901}
   ]
   const title="transflower Pvt Ltd."
   var likes=100;
@@ -20,6 +22,8 @@ function App() {
       <Counter likes={likes}/>
       {/* <Details productName={productName} discription={discription} price={price} stock={stock}/> */}
       <Details productName={product[0].productName} discription={product[0].discription} price={product[0].price} stock={product[0].stock}/>
+      <Details productName={product[1].productName} discription={product[1].discription} price={product[1].price} stock={product[1].stock}/>
+      <Details productName={product[2].productName} discription={product[2].discription} price={product[2].price} stock={product[2].stock}/>
     </div>
   );
 }
