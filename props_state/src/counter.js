0d08c7068c295b0ebe36b32a2646d86deb1e8951
sanika-bhor <bhorsanika0239@gmail.com>
2024-09-11 22:53:23 +0530

    state concept

--- props_state/src/counter.js
+++ props_state/src/counter.js
@@ -1,11 +1,25 @@
 import { useState } from "react";
 
 const Counter = ({ likes }) => {
+  //define state
+  const [count, setCount] = useState(likes);
+
+  const increment = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+    console.log(count);
+  };
+
+
   return (
     <div>
-      <button>-</button>
-      <label>{likes}</label>
-      <button >+</button>
+      <button onClick={decrement}>-</button>
+      <label>{count}</label>
+      <button onClick={increment}>+</button>
     </div>
   );
 };
