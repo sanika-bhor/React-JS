c8a5a86d33bb0ba72a94edf782d4479368359bd0
sanika-bhor <bhorsanika0239@gmail.com>
2025-12-20 17:04:42 +0530

    styling

--- counter/src/Components/Counter.js
+++ counter/src/Components/Counter.js
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
+import "./Counter.css";
+function Counter() {
+  const [count, setCount] = useState(0);
 
-function Counter()
-{
-    const [count,setCount]=useState(0);
-
-    return (
-      <div>
-        <p>count:{count}</p>
-        <button onClick={() => setCount(count + 1)}> Increment</button>
-        <button onClick={() => setCount(count - 1)}> Decrement</button>
+  return (
+    <div class="body">
+      <div class="container">
+        <div class="title">Counter</div>
+        <div class="count" id="count">
+          {count}
+        </div>
+        <div class="buttonGroup">
+          <button class="increment" onClick={() => setCount(count + 1)}>
+            Increment
+          </button>
+          <button class="decrement" onClick={() => setCount(count - 1)}>
+            Decrement
+          </button>
+        </div>
+        <button class="reset" onClick={() => setCount(0)}>
+          Reset
+        </button>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Counter;
