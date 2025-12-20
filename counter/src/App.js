22bb5a4ff65c2ab63b14ed037a8f0fc0274ca277
sanika-bhor <bhorsanika0239@gmail.com>
2025-12-20 15:14:21 +0530

    counter app

---
+++ counter/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Components/Counter.js';
+function App() {
+  return (
+    <div className="App">
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
