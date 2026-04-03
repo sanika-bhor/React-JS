e59d34720da734f51973a1fcc7350f6b42bd7b5e
sanika-bhor <bhorsanika0239@gmail.com>
2026-04-03 12:07:11 +0530

    class component implementation

---
+++ Restart/classcomponent/src/GoodBye.js
@@ -0,0 +1,15 @@
+import React from "react";
+class GoodBye extends React.Component
+{
+    render(){
+        return(
+            <div>
+                <h1>Good Bye Guys See you tomorrow</h1>
+                <button>Hello</button>
+            </div>
+        )
+    }
+    
+}
+
+export default GoodBye;
---
+++ Restart/classcomponent/src/Hello.js
@@ -0,0 +1,14 @@
+import React from "react"
+class Hello extends React.Component
+{
+    render(){
+        return(
+            <div>
+                <h1>Welcome to transflower</h1>
+                <button >Good Bye</button>
+            </div>
+        )
+    }
+}
+
+export default Hello;
